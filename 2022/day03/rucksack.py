1295aeed86c364ab17682085cb86f584f845f594
zrottman <zrottman@example.com>
2023-09-28 20:30:17 -0700

    Finish day 3

---
+++ 2022/day03/rucksack.py
@@ -0,0 +1,39 @@
+alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+items = {c:i+1 for i, c in enumerate(list(alpha))}
+
+# PART 1
+score = 0
+
+with open('input', 'r') as f:
+    while (line := f.readline().strip()):
+        left = set()
+        length = len(line)
+        common = ''
+        for i, char in enumerate(line):
+            if i < length // 2:
+                left.add(char)
+            else:
+                if char in left:
+                    common = char
+                    break
+        score += items[common]
+
+print("Part 1: {}".format(score))
+
+
+# PART 2
+score = 0
+trio = [None] * 3
+count = 0
+        
+with open('input', 'r') as f:
+    while (line := f.readline().strip()):
+        trio[count % 3] = set()
+        for char in line:
+            trio[count % 3].add(char)
+        count += 1
+        if count % 3 == 0:
+            common = list(trio[0].intersection(trio[1]).intersection(trio[2]))[0]
+            score += items[common]
+
+print("Part 2: {}".format(score))
